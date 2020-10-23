01245bb75ac1522ef8f801235292d51bf2b3d67f
seeliang <hi@sihuiliang.com>
2020-10-23 16:01:59 +1100

    init sub in react

--- solutions/redux-raw/src/App.js
+++ solutions/redux-raw/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const {store} = window;
+  const [count, setCount] = useState('0')
+  const update = () => {
+    const count = store.getState().counter.count.toString();
+    setCount(() => count )
+  }
+  useEffect(() => {
+    store.subscribe(update)
+  },[store])
   return (
     <div className="App">
       <h2> react app </h2>
       <h4>sub count</h4>
-      <p> result:</p>
+      <p> result: {count} </p>
       <h4>pub message</h4>
     </div>
   );
