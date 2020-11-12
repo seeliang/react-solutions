65a9cdb53688afe65d991a4c711bedb491ea0a5f
seeliang <hi@sihuiliang.com>
2020-11-12 15:32:24 +1100

    chore: add type to part

--- solutions/webpack/withTS/src/part.tsx
+++ solutions/webpack/withTS/src/part.tsx
@@ -1,3 +1,8 @@
 import React from 'react';
 
-export const Part = ({list}) => <div>{list.map(i => <p key={i}> number {i} </p> )}</div>;
+interface PartProps {
+    list: []
+}
+
+export const Part = ({list} : PartProps) => <div>{list.map(i => <p key={i}> number {i} </p> )}</div>;
+Part.displayName = 'Part'
