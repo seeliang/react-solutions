55a09716ec351f9939a59dc5c189da30f5888918
seeliang <hi@sihuiliang.com>
2022-04-27 13:46:19 +1000

    chore: update demo

--- solutions/recharts/src/App.js
+++ solutions/recharts/src/App.js
@@ -4,36 +4,36 @@ import './App.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid,ReferenceArea } from 'recharts';
 const data = [
   {
-    name: "12:00",
+    name: "21/07 12:00",
     uv: 100,
 
   },
   {
-    name: "12:15",
+    name: "21/07 12:15",
     uv: 100,
   
   },
   {
-    name: "12:30",
+    name: "21/07 12:30",
     uv: 100,
    
   },
   {
-    name: "12:45",
+    name: "21/07 12:45",
     uv: 90,
     
   },
   {
-    name: "13:00",
+    name: "21/07 13:00",
     uv: 70,
    
   },
   {
-    name: "13:15",
+    name: "21/07 13:15",
     uv: 110,
   },
   {
-    name: "13:30",
+    name: "21/07 13:30",
     uv: 140,
   }
 ];
@@ -43,13 +43,14 @@ function App() {
   return (
     <div className="App">
   
-      <LineChart width={500} height={300} data={data}>
-    <XAxis dataKey="name" orientation='top' scale="band"/>
+      <LineChart width={700} height={300} data={data}>
+    <XAxis dataKey="name" orientation='top' scale="band" angle="-10"/>
     <YAxis domain={[60, 160]}/>
     <CartesianGrid stroke="#ddd" strokeDasharray="5 5"/>
-    <Line type="monotone" dataKey="uv" stroke='black' dot={{ stroke: 'black', strokeWidth: 4 }}  />
+    <Line type="monotone" dataKey="uv" stroke='black' dot={{ stroke: 'black', strokeWidth: 8 }}  />
     <ReferenceArea  y1={110} y2={120} fill="orange" strokeOpacity={0.5} />
     <ReferenceArea  y1={120} y2={150} fill="red" strokeOpacity={0.5} />
+    <ReferenceArea  y1={60} y2={65} fill="blue" strokeOpacity={0.5} />
   </LineChart>
     </div>
   );
