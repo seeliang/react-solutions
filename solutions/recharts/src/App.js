f04a75f9243be6a605697c7f7bc89a521e6958c2
seeliang <hi@sihuiliang.com>
2022-04-27 11:38:05 +1000

    feat: add reference and XAxis location

--- solutions/recharts/src/App.js
+++ solutions/recharts/src/App.js
@@ -1,39 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid,ReferenceArea } from 'recharts';
 const data = [
   {
-    name: "",
+    name: "12:00",
     uv: 100,
 
   },
   {
-    name: "",
+    name: "12:15",
     uv: 100,
   
   },
   {
-    name: "",
+    name: "12:30",
     uv: 100,
    
   },
   {
-    name: "",
+    name: "12:45",
     uv: 90,
     
   },
   {
-    name: "",
+    name: "13:00",
     uv: 70,
    
   },
   {
-    name: "",
+    name: "13:15",
     uv: 110,
   },
   {
-    name: "Page G",
+    name: "13:30",
     uv: 140,
   }
 ];
@@ -44,11 +44,13 @@ function App() {
     <div className="App">
   
       <LineChart width={500} height={300} data={data}>
-    <XAxis dataKey="name"/>
-    <YAxis domain={[40, 200]}/>
+    <XAxis dataKey="name" orientation='top'/>
+    <YAxis domain={[40, 160]}/>
     <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
     <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+    <ReferenceArea  y1={110} y2={120} fill="orange" strokeOpacity={0.3} />
+    <ReferenceArea  y1={120} y2={150} fill="red" strokeOpacity={0.3} />
   </LineChart>
     </div>
   );
