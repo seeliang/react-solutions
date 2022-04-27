e40bb768a87481487749a4555d565b109e253e36
seeliang <hi@sihuiliang.com>
2022-04-27 15:11:27 +1000

    feat: stack

--- solutions/recharts/src/App.js
+++ solutions/recharts/src/App.js
@@ -38,20 +38,30 @@ const data = [
   }
 ];
 
+const Sample = ({showTitle}) => (
+  <div className="table">
+  <span className="cell title">Heart Beat Rate</span>
+  <span className="cell">
+    <LineChart width={700} height={300} data={data}>
+  <XAxis dataKey={"name"} hide={!showTitle} orientation='top' scale="band" angle="-8"/>
+  <YAxis domain={[60, 160]}/>
+  <CartesianGrid stroke="#ddd" strokeDasharray="5 5"/>
+  <Line type="monotone" dataKey="uv" stroke='black' dot={{ stroke: 'black', strokeWidth: 8 }}  />
+  <ReferenceArea  y1={110} y2={120} fill="orange" strokeOpacity={0.5} />
+  <ReferenceArea  y1={120} y2={150} fill="red" strokeOpacity={0.5} />
+  <ReferenceArea  y1={60} y2={65} fill="blue" strokeOpacity={0.5} />
+</LineChart>
+</span>
+</div>
+);
+
 
 function App() {
   return (
     <div className="App">
   
-      <LineChart width={700} height={300} data={data}>
-    <XAxis dataKey="name" orientation='top' scale="band" angle="-10"/>
-    <YAxis domain={[60, 160]}/>
-    <CartesianGrid stroke="#ddd" strokeDasharray="5 5"/>
-    <Line type="monotone" dataKey="uv" stroke='black' dot={{ stroke: 'black', strokeWidth: 8 }}  />
-    <ReferenceArea  y1={110} y2={120} fill="orange" strokeOpacity={0.5} />
-    <ReferenceArea  y1={120} y2={150} fill="red" strokeOpacity={0.5} />
-    <ReferenceArea  y1={60} y2={65} fill="blue" strokeOpacity={0.5} />
-  </LineChart>
+    <Sample showTitle={true}/>
+    <Sample/>
     </div>
   );
 }
