c56bab42cf2f68d407fa57d1cb73b8fe497bacae
seeliang <hi@sihuiliang.com>
2022-04-28 11:13:51 +1000

    feat: range

--- solutions/recharts/src/App.js
+++ solutions/recharts/src/App.js
@@ -6,35 +6,37 @@ const data = [
   {
     name: "21/07 12:00",
     uv: 100,
-
+    kk: 89,
   },
   {
     name: "21/07 12:15",
     uv: 100,
-  
+    kk: 67
   },
   {
     name: "21/07 12:30",
     uv: 100,
-   
+    kk: 90
   },
   {
     name: "21/07 12:45",
     uv: 90,
-    
+    kk: 86
   },
   {
     name: "21/07 13:00",
     uv: 70,
-   
+    kk: 64
   },
   {
     name: "21/07 13:15",
     uv: 110,
+    kk: 85
   },
   {
     name: "21/07 13:30",
     uv: 140,
+    kk: 110
   }
 ];
 
@@ -44,9 +46,10 @@ const Sample = ({showTitle}) => (
   <span className="cell">
     <LineChart width={700} height={300} data={data}>
   <XAxis dataKey={"name"} hide={!showTitle} orientation='top' scale="band" angle="-8"/>
-  <YAxis domain={[60, 160]}/>
+  <YAxis domain={[60, 160]} tickCount="10"/>
   <CartesianGrid stroke="#ddd" strokeDasharray="5 5"/>
   <Line type="monotone" dataKey="uv" stroke='black' dot={{ stroke: 'black', strokeWidth: 8 }}  />
+  <Line type="monotone" dataKey="kk" stroke='green' dot={{ stroke: 'green', strokeWidth: 8 }}  />
   <ReferenceArea  y1={110} y2={120} fill="orange" strokeOpacity={0.5} />
   <ReferenceArea  y1={120} y2={150} fill="red" strokeOpacity={0.5} />
   <ReferenceArea  y1={60} y2={65} fill="blue" strokeOpacity={0.5} />
