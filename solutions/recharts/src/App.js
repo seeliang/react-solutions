c6bc2f2f80f90da9b01c0ad302ac2eaf7659722c
seeliang <hi@sihuiliang.com>
2022-05-13 09:23:48 +1000

    chore: relocate the data to 0 to correct tick count set

--- solutions/recharts/src/App.js
+++ solutions/recharts/src/App.js
@@ -80,10 +80,13 @@ function round5 (x) {
 }
 
 function addDisplayToData (data) {
-  return data.map(i => ({...i, display: {max: round5(i.max), min: round5(i.min) } }) )
+  return data.map(i => ({...i, 
+    display: {
+    max: i.max > YDomain[1] ? 225 : round5(i.max - YDomain[0]), 
+    min: i.min < YDomain[0] ? 65 : round5(i.min - YDomain[0]) 
+    } 
+  }) )
 }
-
-
 
 class CustomizedLabel extends PureComponent {
   render() {
@@ -135,22 +138,27 @@ const ModifyTooltip = (props) => {
 
   const safeRange = [80,180]
 
+
+
   function formatYAxis(value) {
 
-    if(value === safeRange[1]) return "Over " + safeRange[1]
-    if (value < safeRange[1] && value > safeRange[0]) return value
-    if(value === safeRange[0]) return "Under " + safeRange[0] 
+    const start = 0;
+    const end = YDomain[1] - YDomain[0]
+
+    if(value === end ) return "Over " + YDomain[1]
+    if (value < end && value > start) return value + YDomain[0]
+    if(value === start) return "Under " + YDomain[0] 
     return ""
   }
 
-const tickCount = (Math.ceil((YDomain[1] - YDomain[0])/10) + 2).toString(10);
+const tickCount = (Math.ceil((safeRange[1] - safeRange[0])/10) ).toString(10);
 
 console.log(tickCount);
 
 const ConditionLabel = (props) => {
   const {value, x, y} = props;
 
-  if (value < safeRange[1] && value > safeRange[0]) {
+  if (value < safeRange[1] - YDomain[0] && value > safeRange[0]- YDomain[0]) {
     return <circle r="7" fill="green" cx={x} cy={y}/>;
   };
   return (<>
@@ -159,29 +167,33 @@ const ConditionLabel = (props) => {
   </>)
 }
 
+const YAxisSharedProps = {
+  tickCount,
+  // domain: YDomain,
+}
+
 const Sample = () => (
   <div className="table">
   <span className="sticky cell table">
     <span className=" title">Heart Beat Rate</span>
     <span className="cell" >
       <LineChart width={120} height={300} data={addDisplayToData(data)}>
-        <YAxis domain={YDomain} width={110} tickFormatter={formatYAxis} tickCount={tickCount} interval="preserveStartEnd"/>
+        <YAxis {...YAxisSharedProps} width={110} tickFormatter={formatYAxis} />
       </LineChart>
     </span>
   </span>
   <span className="cell">
     <LineChart width={1700} height={300} data={addDisplayToData(data)}>
-    <ReferenceArea  y1={150} y2={safeRange[1]} fill="orange" strokeOpacity={0.5} />
-  <ReferenceArea  y1={safeRange[1]} y2={YDomain[1]} fill="red" strokeOpacity={0.5} />
-  <ReferenceArea  y1={YDomain[0]} y2={safeRange[0]} fill="blue" strokeOpacity={0.5} />
-  <XAxis dataKey={"name"} hide={true} orientation='top' scale="band" angle="-8"/>
-  <CartesianGrid stroke="#ddd"/>
-  <Tooltip content={ModifyTooltip}/>
-  <YAxis domain={YDomain} tickCount={tickCount}  hide={true}/>
-  <Line type="monotone" dataKey="display.max" stroke='black' dot={{ stroke: 'black', strokeWidth: 25 }}  label={<CustomizedLabel data={data} />}  />
-  <Line type="monotone" dataKey="display.min" stroke='green' label={<ConditionLabel data={data} />}   />
-
-</LineChart>
+      <ReferenceArea  y1={150} y2={safeRange[1]} fill="orange" strokeOpacity={0.5} />
+      <ReferenceArea  y1={safeRange[1]} y2={YDomain[1]} fill="red" strokeOpacity={0.5} />
+      <ReferenceArea  y1={YDomain[0]} y2={safeRange[0]} fill="blue" strokeOpacity={0.5} />
+      <XAxis dataKey={"name"} hide={true} orientation='top' scale="band" angle="-8"/>
+      <CartesianGrid stroke="#ddd"/>
+      <Tooltip content={ModifyTooltip}/>
+      <YAxis {...YAxisSharedProps} tickFormatter={formatYAxis}/>
+      <Line type="monotone" dataKey="display.max" stroke='black' dot={{ stroke: 'black', strokeWidth: 25 }}  label={<CustomizedLabel data={data} />}  />
+      <Line type="monotone" dataKey="display.min" stroke='green' label={<ConditionLabel data={data} />} />
+  </LineChart>
 </span>
 </div>
 );
