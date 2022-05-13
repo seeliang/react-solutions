bab0c3a1f2e6a5d1e43313a82b6b48235d481bc9
seeliang <hi@sihuiliang.com>
2022-05-13 16:00:23 +1000

    feat: add single

--- solutions/recharts/src/App.js
+++ solutions/recharts/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Double, {TableHead} from './Double';
 
+import Number from './SingleNumber'
+
 
 function App() {
   return (
     <div className="App">
       <TableHead />
     <Double />
+    <Number />
     </div>
   );
 }
