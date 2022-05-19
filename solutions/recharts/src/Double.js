c29e14c497e464ef0100d90d64370e2d40a2879d
seeliang <hi@sihuiliang.com>
2022-05-19 14:25:19 +1000

    chore: relocate data

--- solutions/recharts/src/Double.js
+++ solutions/recharts/src/Double.js
@@ -2,78 +2,8 @@ import { PureComponent } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid,ReferenceArea, ReferenceLine, Tooltip } from 'recharts';
 import {XAxisJustifiedProps, timeToNum, XDisplayReset, ErrorInputProps} from './SingleFloat';
 
-const data = [
-  {
-    name: "12:00",
-    max: 100,
-    min: 89,
-  },
-  {
-    name: "12:15",
-    max: 100,
-    min: 35
-  },
-  {
-    name: "12:30",
-    max: 100,
-    min: 90
-  },
-  {
-    name: "12:38",
-    max: 90,
-    min: 86
-  },
-  {
-    name: "13:00",
-    max: 77,
-    min: 64
-  },
-  {
-    name: "13:15",
-    max: 110,
-    min: 85
-  },
-  {
-    name: "13:30",
-    max: 308,
-    min: 110
-  },
-  {
-    name: "14:00",
-    max: 100,
-    min: 89,
-  },
-  {
-    name: "14:15",
-    max: 100,
-    min: 67
-  },
-  {
-    name: "14:30",
-    max: 100,
-    min: 90
-  },
-  {
-    name: "14:38",
-    max: 90,
-    min: 86
-  },
-  {
-    name: "15:00",
-    max: 70,
-    min: 64
-  },
-  {
-    name: "15:15",
-    max: 110,
-    min: 85
-  },
-  {
-    name: "15:30",
-    max: 140,
-    min: 110
-  }
-];
+import { data } from './data'
+
 
 const displayReset = (value) => value - YDomain[0];
 
