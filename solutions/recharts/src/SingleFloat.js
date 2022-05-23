2c4a4c948a44b6fe5cc2d4d42a9af4752b2a1210
seeliang <hi@sihuiliang.com>
2022-05-24 09:14:32 +1000

    chore: YAxisFunc

--- solutions/recharts/src/SingleFloat.js
+++ solutions/recharts/src/SingleFloat.js
@@ -2,29 +2,15 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid,ReferenceArea, Tooltip, Re
 import {XAxisJustifiedProps, ErrorInputProps, getResetTime,XWidth} from './XAxisFunc';
 import {DotLabel} from './Label';
 import { data } from './data';
-import ModifyTooltip from './Tooltip'
+import ModifyTooltip from './Tooltip';
+import {getYAxisSharedProps, formatData} from './YAxisFunc'
 
 const YDisplayReset = (value) => value - YDomain[0];
-
-function roundToHalf (x) {
-  return Math.ceil(x/YGap)*YGap - YGap / 2
-}
-
-function formatData (value)  {
-  if (value < YDomain[0]) {
-    return YGap / 2
-  }
-  if (value > YDomain[1]) {
-    return YDisplayReset(YDomain[1]) - YGap / 2;
-  }
-
-  return roundToHalf(YDisplayReset(value))
-}
 
 function addDisplayToData (data) {
   return data.map(i => ({...i, 
     display: {
-    min: formatData(i.float.min),
+    min: formatData({domain:YDomain, gap: YGap, value:i.float.min}),
     time: getResetTime(i.name)
     } 
   }) )
@@ -32,16 +18,6 @@ function addDisplayToData (data) {
   const YDomain = [34,42];
   const YGap = 0.5
   const safeRange = [36.0,38.0]
-
-  function formatYAxis(value) {
-    const start = YGap;
-    const end = parseFloat(YDisplayReset(YDomain[1] - YGap).toFixed(1));
-
-    if(value === end ) return "Over " + (YDomain[1] - YGap)
-    if (value < end && value > start) return value + YDomain[0]
-    if(value === start) return "Under " + (YDomain[0] + YGap )
-    return ""
-  }
 
   const CustomizedTooltip = ({payload}) => {
     const render = payload.map(i => i.payload);
@@ -57,15 +33,7 @@ function addDisplayToData (data) {
     )
   }
 
-
-const YTickCount = (Math.ceil(YDisplayReset(YDomain[1])/YGap) + 2).toString(10);
-
-const YAxisSharedProps = {
-  tickCount: YTickCount,
-  domain:[0,YDisplayReset(YDomain[1])],
-  tickFormatter: formatYAxis
-}
-
+const YAxisSharedProps = getYAxisSharedProps({gap: YGap, domain: YDomain})
 
 const LineChartProps = {
   height: 500,
@@ -75,7 +43,7 @@ const LineChartProps = {
  const Sample = () => (
   <div className="table">
   <span className="sticky cell table">
-    <span className=" title">Data range</span>
+    <span className=" title">Heart Beat Rate</span>
     <span className="cell" >
       <LineChart width={120} {...LineChartProps}>
         <YAxis {...YAxisSharedProps} width={110} />
