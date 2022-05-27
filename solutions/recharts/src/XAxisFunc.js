95b6e58c17ad4212ce77e4f9ff2bfb59993622f6
seeliang <hi@sihuiliang.com>
2022-05-27 11:51:16 +1000

    fix: month and leading zero

--- solutions/recharts/src/XAxisFunc.js
+++ solutions/recharts/src/XAxisFunc.js
@@ -7,6 +7,7 @@ const regexGetDigits = /\D/g;
 const timeToNum = (string) => {
   const raw = string.split(regexGetDigits).map( i => parseInt(i));
   const mark = new Date( raw[2],raw[1] - 1 ,raw[0],raw[3],raw[4]);
+  console.log(mark);
   return  Date.parse(mark)
 }
 
@@ -20,7 +21,7 @@ function formatXAxis(modifiedValue) {
   const dateNumber = modifiedValue + timeToNum(data[0].name);
   const date = new Date(dateNumber);
   const isNewDate = date.getHours() + date.getMinutes() === 0;
-  const dateDisplay = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} `;
+  const dateDisplay = `${addLeadZero(date.getDate())}/${addLeadZero(date.getMonth() + 1)}/${date.getFullYear()} `;
   return `${isNewDate? dateDisplay: '' }${addLeadZero(date.getHours())}:${addLeadZero(date.getMinutes())}`
 }
 
