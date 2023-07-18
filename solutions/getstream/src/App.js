9cc73ac01649fc7938370e3e07d0743ba0e3c87d
See way Liang <hi@sihuiliang.com>
2023-07-18 15:05:00 +1000

    chore: init client

--- solutions/getstream/src/App.js
+++ solutions/getstream/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+} from 'stream-chat-react';
+import credentials from './credentials';
+
+const { apiKey } = credentials
 
 function App() {
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    const newClient = new StreamChat(apiKey);
+
+
+    setClient(newClient);
+
+
+  }, []);
+
+  if (!client) return null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chat client={client}>
+
+    </Chat>
   );
 }
 
