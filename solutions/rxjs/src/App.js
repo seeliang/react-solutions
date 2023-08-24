ea2fe0ee401d82a092c8caf00c34e62dca4d9cae
See way Liang <hi@sihuiliang.com>
2023-08-25 09:23:03 +1000

    feat: idea case

--- solutions/rxjs/src/App.js
+++ solutions/rxjs/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([])
+  const handleAddNewItem = () => {
+    setList([1, ...list])
+  }
+  const handleLoadMoreItem = () => {
+    setList([...list, 10])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={handleAddNewItem}>add new item</button>
+        <button onClick={handleLoadMoreItem}>load 10 more items</button>
+      </div>
+      <p>happy path: there is not race condition</p>
+      <p>{JSON.stringify(list)}</p>
     </div>
   );
 }
