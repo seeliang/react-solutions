6198338ffce138114d9d95af6802feb2df209ef6
See way Liang <hi@sihuiliang.com>
2023-08-25 15:15:49 +1000

    feat: debounce click with rx

--- solutions/rxjs/src/App.js
+++ solutions/rxjs/src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { debounceTime } from 'rxjs/operators'
+import { Subject } from 'rxjs'
+
+const useDebounce = (time, initialValue) => {
+  const [value, setValue] = useState(initialValue)
+  const [values] = useState(() => new Subject())
+  useEffect(() => {
+    const sub = values.pipe(debounceTime(time)).subscribe(setValue)
+    return () => sub.unsubscribe()
+  }, [time, values])
+  return [value, (v) => values.next(v)]
+}
+
+
+// const useBuffer = (time, initialValue) => {
+//   const [value, setValue] = useState(initialValue)
+//   const [values] = useState(() => new Subject())
+//   useEffect(() => {
+//     console.log(value)
+//     const sub = values.pipe(
+//       buffer(values.pipe(throttleTime(time)))).subscribe(setValue)
+//     return () => sub.unsubscribe()
+
+//   }, [time, values])
+//   return [value, (v) => values.next(v)]
+// }
 
 function App() {
-  const [list, setList] = useState([])
+  const [list, setList] = useDebounce(1000, [])
   const handleAddNewItem = () => {
     setList([1, ...list])
   }
@@ -16,7 +43,7 @@ function App() {
         <button onClick={handleAddNewItem}>add new item</button>
         <button onClick={handleLoadMoreItem}>load 10 more items</button>
       </div>
-      <p>happy path: there is not race condition</p>
+      <p>debounce: only accept last one click in a min</p>
       <p>{JSON.stringify(list)}</p>
     </div>
   );
